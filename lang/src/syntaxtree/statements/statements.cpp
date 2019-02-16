e90106f6cc6f59c6eebd6203814b3c1404b2392c
Paul Kreymborg <paulkreymborg@Pauls-MacBook-Pro.local>
2019-02-16 14:54:38 -0800

    Working on more syntax nodes

---
+++ lang/src/syntaxtree/statements/statements.cpp
@@ -0,0 +1,2 @@
+#include "./statements.hpp"
+
---
+++ lang/src/syntaxtree/statements/statements.hpp
@@ -0,0 +1,21 @@
+#ifndef STATEMENTS_HPP
+#define STATEMENTS_HPP
+
+#include <variant>
+
+namespace syntax_tree::statements
+{
+    class ComplexExpressionStatement
+    {
+    public:
+        ComplexExpressionStatement();
+    };
+    
+    class StatementContainer
+    {
+    public:
+        StatementContainer();
+    };
+}
+
+#endif
